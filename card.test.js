7c595ff7ac055699e66b46b31b63b9744f4bd508
Chrishaun Jones <ckjones1@student.fullsail.edu>
2024-11-05 18:11:40 -0500

    added three more tests

--- card.test.js
+++ card.test.js
@@ -4,4 +4,16 @@ describe("obscure credit card numbers", () => {
   test("Obscures all but last 4 digits for a valid 16-digit credit card", () => {
     expect(cardValidate("1234567812345678")).toBe("************5678");
   });
+
+  test("Returns Invalid Credit Card for a 8-digit input", () => {
+    expect(cardValidate("89003829")).toBe("Invalid Credit Card");
+  });
+
+  test("Obscures all but last 4 digits for a valid 13-digit credit card", () => {
+    expect(cardValidate("4688276781258")).toBe("*********1258");
+  });
+
+  test("Returns 'Invalid Credit Card' for a 19-digit input", () => {
+    expect(cardValidate("2453782938471879273")).toBe("Invalid Credit Card");
+  });
 });
--- cardValidate.js
+++ cardValidate.js
@@ -1,11 +1,11 @@
 const cardValidate = (num) => {
   //Check if the input is a string and contains only digits
   //Check if the length of the credit card number is within valid range
-  if (num == null || num.length < 12 || num.length > 16) {
+  if (num.length < 12 || num.length > 16) {
     return "Invalid Credit Card";
   }
 
-  const maskedCard = "*".repeat(num.length - 4);
+  const maskedCard = "*".repeat(num.length - 4) + num.slice(-4);
   return maskedCard;
 };
 
